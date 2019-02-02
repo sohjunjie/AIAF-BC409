769b492ae6dfc88805961c564df05437ba2adecb
sohjunjie <junjie.soh93@gmail.com>
2019-02-02 13:30:41 +0800

    combine price+news dataset

---
+++ create_dataset.py
@@ -0,0 +1,15 @@
+"""
+Join DJIA price dataset with reddit news by common date
+
+(Pandas.DataFrame) 1989 rows x 32 cols
+"""
+
+import pandas as pd
+
+
+DJIA_PRICE = pd.read_csv('data/DJIA_table.csv', index_col='Date')
+DJIA_NEWS = pd.read_csv('data/Combined_News_DJIA.csv', index_col='Date')
+DJIA_PRICE_NEWS = pd.merge(DJIA_PRICE, DJIA_NEWS, left_index=True, right_index=True)
+DJIA_PRICE_NEWS.to_csv('data/DJIA_WITH_NEWS.csv')
+
+DJIA_PRICE_NEWS = pd.read_csv('data/DJIA_WITH_NEWS.csv', index_col='Date')
