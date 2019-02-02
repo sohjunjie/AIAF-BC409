d53e1b3d7ed5377bc5cbcd839f3dd0f43cfbc7cb
sohjunjie <junjie.soh93@gmail.com>
2019-02-03 02:19:57 +0800

    standardize feature function in utils.py

--- utils.py
+++ utils.py
@@ -9,14 +9,21 @@ def load_word2vec():
 
 
 def create_vector(word, word2vec, word_vector_size=100):
-    # if the word is missing from word2vec, create some fake vector and store in word2vec!
-    vector = np.random.uniform(0.0,1.0,(word_vector_size,))
+    """ if the word is missing from word2vec, create some fake vector and store in word2vec """
+    vector = np.random.uniform(-1.0, 1.0, (word_vector_size,))
     word2vec.wv.add(word, vector)
     return vector
 
 
-def process_word(word, word2vec, vocab, ivocab, word_vector_size, to_return="word2vec", silent=False):
+def process_word(word, word2vec):
     """ return word embedding from word2vec model given a word token """
-    if not word in word2vec.wv.vocab
-        create_vector(word, word2vec, word_vector_size, silent)
+    if not word in word2vec.wv.vocab:
+        create_vector(word, word2vec)
     return word2vec.wv.get_vector(word)
+
+
+def standardize_features(dataset, features):
+    """ standardize feature of pandas dataframe to zero mean, unit standard deviation """
+    for f in features:
+        dataset[f] = (dataset[f] - dataset[f].mean()) / dataset[f].std()
+    return dataset
