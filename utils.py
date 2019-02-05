7dfbde7f86556d2e14a029505d1d44d2958dae7a
sohjunjie <junjie.soh93@gmail.com>
2019-02-05 15:01:05 +0800

    perform news sequence zero padding

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
 from gensim.models import Word2Vec
 from gensim.models.word2vec import Text8Corpus
 import numpy as np
+from pymongo import MongoClient
+
+
+def _get_news_maxseq_len():
+    """ 
+    find the max news sequence len to determine pad length
+    > precomputed len = 64
+    """
+    client = MongoClient('localhost', 27017)
+    db = client.djia_news_dataset
+    db_tbl_price_news = db.price_news
+    maxlen = 0
+    news_col_name = ['Top'+str(x) for x in range (1, 26)]
+    res = db_tbl_price_news.find()
+    for r in res:
+        maxlen = max(maxlen, max([len(r[cn]) for cn in news_col_name]))
+    return maxlen
 
 
 def standardize_features(dataset, features):
@@ -15,27 +32,44 @@ def load_word2vec():
     return model_gs
 
 
-def create_vector(word, word2vec, word_vector_size=100):
+def create_vector(word, word2vec, word_vector_size):
     """ if the word is missing from word2vec, create some fake vector and store in word2vec """
     vector = np.random.uniform(-1.0, 1.0, (word_vector_size,))
     word2vec.wv.add(word, vector)
     return vector
 
 
-def process_word(word, word2vec):
+def process_word(word, word2vec, word_vector_size=100):
     """ return word embedding from word2vec model given a word token """
     if not word in word2vec.wv.vocab:
-        create_vector(word, word2vec)
-    return (word2vec.wv.get_vector(word)).tolist()
+        create_vector(word, word2vec, word_vector_size)
+    return word2vec.wv.get_vector(word)
 
 
-def process_sentence(sentence, word2vec):
-
+def process_sentence(sentence, word2vec, truncate=True, pad_length=64, word_vector_size=100):
+    """
+    64 is the precomputed maxseq length for a given news
+    """
     if not type(sentence) is str:
-        return []
+        sent_vector = np.zeros(shape=(64, 100), dtype='float')
+        return sent_vector.tolist()
 
     sent = sentence.lower().split(' ')
     sent = [w for w in sent if len(w) > 0]
-    sent_vector = [process_word(word=w,
-                                word2vec=word2vec) for w in sent]
-    return sent_vector
+
+    sent_vector = np.array([process_word(word=w,
+                                        word2vec=word2vec,
+                                        word_vector_size=100) for w in sent])
+    if truncate:
+        sent_vector = sent_vector[:pad_length]
+
+    a, b = sent_vector.shape
+
+    if pad_length - a < 0 or word_vector_size - b < 0:
+        raise Exception('sentence vector shape out of bound')
+
+    sent_vector = np.pad(array=sent_vector,
+                         pad_width=((0, pad_length-a), (0, word_vector_size-b)),
+                         mode='constant')
+
+    return sent_vector.tolist()
